431605e31c2dfcc1c051e0a3488a99dacb147988
Rennan Rezende <rennan765@gmail.com>
2018-07-28 17:05:10 -0300

    Criação do diagrama de classes.

--- EstoqueMangas.Core/Entities/Join/AutorManga.cs
+++ EstoqueMangas.Core/Entities/Join/AutorManga.cs
@@ -4,9 +4,9 @@ namespace EstoqueMangas.Core.Entities.Join
     public class AutorManga
     {
         #region Atributos 
-        public Guid IdManga { get; private set; }
+        public Guid MangaId { get; private set; }
         public Manga Manga { get; private set; }
-        public Guid IdAutor { get; private set; }
+        public Guid AutorId { get; private set; }
         public Autor Autor { get; private set; }
         #endregion 
     }
