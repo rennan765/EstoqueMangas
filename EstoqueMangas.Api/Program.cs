e46c988d131ffbcb27578bb881c3ecacceb1997c
Rennan Rezende <rennan765@gmail.com>
2018-09-12 20:29:00 -0300

    Adicionado método de integração com o IIS.

--- EstoqueMangas.Api/Program.cs
+++ EstoqueMangas.Api/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace EstoqueMangas.Api
 {
@@ -31,7 +24,7 @@ namespace EstoqueMangas.Api
         /// <param name="args">Arguments.</param>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   //.UseUrls("http://localhost:5050")
+                   .UseIISIntegration()
                    .UseStartup<Startup>();
     }
 }
