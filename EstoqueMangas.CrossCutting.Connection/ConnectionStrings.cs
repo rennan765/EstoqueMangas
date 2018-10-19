d5059ab2414f7fa8a05596ccd3ad62ecac676e19
Rennan Rezende <rennan765@gmail.com>
2018-10-18 22:02:46 -0300

    Adição do ConnectionStrings.cs

---
+++ EstoqueMangas.CrossCutting.Connection/ConnectionStrings.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EstoqueMangas.CrossCutting.Connection
+{
+    public class ConnectionStrings
+    {
+        #region Atributos
+        private readonly IConfiguration _configuration;
+        #endregion
+
+        #region Construtores
+        public ConnectionStrings()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+        }
+        #endregion 
+
+        #region Métodos
+        public string MySql()
+        {
+            return _configuration["ConnectionStrings:MySql"];
+        }
+
+        public string SqlServer()
+        {
+            return _configuration["ConnectionStrings:SqlServer"];
+        }
+        #endregion 
+    }
+}
